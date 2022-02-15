ff9d3f32c422f033e4c0e26d662d8840e790396f
LefterLuci <99748961+LefterLuci@users.noreply.github.com>
2022-02-15 18:23:55 +0200

    Add files via upload

---
+++ swtich.c
@@ -0,0 +1,20 @@
+#include stdio.h
+#include stdlib.h
+#include math.h
+
+int main()
+{
+    int count;
+    int index = 0;
+
+    printf(Introduceti un numarn);
+    scanf(%d,&count);
+
+    while(index!=count+1){
+        printf(%d ,index);
+        index++;
+
+    }
+
+    return 0;
+}
